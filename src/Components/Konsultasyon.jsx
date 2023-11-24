02d6c051c1808da7855459a916ff2410b605f5d7
ascyazilim <yazilimasc@gmail.com>
2023-11-24 14:44:53 +0300

    template added

--- src/Components/Konsultasyon.jsx
+++ src/Components/Konsultasyon.jsx
@@ -220,7 +220,7 @@ export const Konsultasyon = ({ onClose }) => {
           </div>
         </div>
       </div>
-      <div className="karsilama-not">
+      {/* <div className="karsilama-not">
         <div className="karsilama-not-button">
           <button
             className={`istek-button ${noteType === "istek" ? "active" : ""}`}
@@ -242,7 +242,7 @@ export const Konsultasyon = ({ onClose }) => {
             name="istek-note"
             id="istek-note"
             cols="122"
-            rows="8"
+            rows="6"
             placeholder="İstek Notu girin"
             value={istekNotu}
             onChange={(e) => setIstekNotu(e.target.value)}
@@ -252,7 +252,50 @@ export const Konsultasyon = ({ onClose }) => {
             name="karsilama-note"
             id="karsilama-note"
             cols="122"
-            rows="8"
+            rows="6"
+            placeholder="Karşılama Notu girin"
+            value={karsilamaNotu}
+            onChange={(e) => setKarsilamaNotu(e.target.value)}
+          ></textarea>
+        )}
+      </div> */}
+      <div className="tabstrip-konsultasyon">
+        <button
+          className={
+            noteType === "istek" ? "tabstrip-button active" : "tabstrip-button"
+          }
+          onClick={() => setNoteType("istek")}
+        >
+          İstek Notu
+        </button>
+        <button
+          className={
+            noteType === "karsilama"
+              ? "tabstrip-button active"
+              : "tabstrip-button"
+          }
+          onClick={() => setNoteType("karsilama")}
+        >
+          Karşılama Notu
+        </button>
+      </div>
+      <div className="karsilama-not">
+        {noteType === "istek" ? (
+          <textarea
+            name="istek-note"
+            id="istek-note"
+            cols="122"
+            rows="6"
+            placeholder="İstek Notu girin"
+            value={istekNotu}
+            onChange={(e) => setIstekNotu(e.target.value)}
+          ></textarea>
+        ) : (
+          <textarea
+            name="karsilama-note"
+            id="karsilama-note"
+            cols="122"
+            rows="6"
             placeholder="Karşılama Notu girin"
             value={karsilamaNotu}
             onChange={(e) => setKarsilamaNotu(e.target.value)}
