225f726b2dd74370bb38545eef0b5ae4dcc4e9f3
ascyazilim <yazilimasc@gmail.com>
2023-12-13 11:09:41 +0300

    updated DicomViewer

--- src/Components/DicomViewer.jsx
+++ src/Components/DicomViewer.jsx
@@ -1,31 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react';
 import  cornerstone from 'cornerstone-core';
 import * as cornerstoneTools from 'cornerstone-tools';
-//import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
-// import * as dicomImageLoader from 'dicom-image-loader';
 import cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
 import * as dicomParser from 'dicom-parser';
 
 
 cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
 cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
-
 cornerstoneTools.external.cornerstone = cornerstone;
 
+//Web Worker Manager initialization
+cornerstoneWADOImageLoader.webWorkerManager.initialize({
+    maxWebWorkers: navigator.hardwareConcurrency || 1,
+    startWebWorkersOnDemand: true,
+    webWorkerPath: '/path/to/cornerstoneWADOImageLoaderWebWorker.js',
+    taskConfiguration: {
+        'decodeTask': {
+            loadCodecsOnStartup: true,
+            initializeCodecsOnStartup: false,
+            codecsPath: '/path/to/cornerstoneWADOImageLoaderCodecs.js',
+            usePDFJS: false,
+        }
+    }
+});
 
-// dicomImageLoader.external.cornerstone = cornerstone;
-// dicomImageLoader.external.dicomParser = dicomParser; 
 
 function DicomViewer() {
 
-    //OHIFViewer-----------------------------------------------------------
-    // const ohifViewerUrl = 'http://localhost:8080';
-    // const dicomFile = '/00000001';
-    //OHIF Viewer URL + DICOM dosya adını birleştirme
-    // const viewerUrl = '${ohifViewerUrl}/viewer/${dicomFile}';
-    //-------------------------------------------------------------
-
-    //Eski cornerstone----------------------------------------- 
     const dicomImageRef = useRef(null);
     const dicomUrl = process.env.PUBLIC_URL + '/00000001'; // DICOM dosyasının URL'si
 
